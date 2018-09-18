b39438c723f653791d425f71ece14faa1e5f59bc
Mike Smith <mikesmithlab@gmail.com>
2018-09-18 13:59:14 +0100

    Add files via upload

---
+++ fitting.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import optimize
+
+
+'''
+Fit functions to a series of points
+'''
+    
+def calc_R(x,y, xc, yc):
+    """ calculate the distance of each 2D points from the center (xc, yc) """
+    return np.sqrt((x-xc)**2 + (y-yc)**2)
+
+def f(c, x, y):
+    """ calculate the algebraic distance between the data points and the mean circle centered at c=(xc, yc) """
+    Ri = calc_R(x, y, *c)
+    return Ri - Ri.mean()
+
+def leastsq_circle(x,y):
+    # coordinates of the barycenter
+    x_m = np.mean(x)
+    y_m = np.mean(y)
+    center_estimate = x_m, y_m
+    center, ier = optimize.leastsq(f, center_estimate, args=(x,y))
+    xc, yc = center
+    Ri       = calc_R(x, y, *center)
+    R        = Ri.mean()
+    #residu   = np.sum((Ri - R)**2)
+    return xc, yc, R
+
