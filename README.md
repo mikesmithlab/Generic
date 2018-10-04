950021a68564af4ef0dec71caa461ff128380c11
James Downs <james.downs@nottingham.ac.uk>
2018-10-04 15:46:25 +0100

    Added stepper.Stepper which inherits from arduino.Arduino and .ino file

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 This is a repository for all the highly generic code that will be reused in project after project. It consists of the following modules
 - Camera --> Allows taking images and videos with the different cameras in the lab with generic user interface
 - Video --> Allows reading writing videos
-- stepper --> Contains class Stepper which handles Serial communication with Arduino running newStepperBoard.ino to control up to 2 stepper motors
+- stepper --> Contains class DualStepper which handles Serial communication with Arduino running newStepperBoard.ino to control up to 2 stepper motors.
+              Contains class Stepper which controls a number of stepper motors with Arduino running StepperMotor.ino
 - load_cell --> Contains class LoadCell which inherits from arduino.Arduino to read the value of a load cell from an Arduino running LoadCellSingle.ino
 - arduino --> Contains class Arduino which handles Serial communication with Arduinos
+
+
+Also contains a number of arduino sketches:
+- LoadCellSingle.ino --> Allows the reading of load cell values by the load_cell.LoadCell class
+- newStepperBoard.ino --> Allows the movement of two stepper motors using the stepper.DualStepper class
+- StepperMotor.ino --> Allows the movement of a number of stepper motors using the stepper.Stepper class (needs additional stepper motors adding)
--- stepper.py
+++ stepper.py
@@ -9,10 +9,10 @@ Created on Tue Aug 14 10:49:19 2018
 import sys
 import serial
 import time
-
+import arduino
 
 class DualStepper:
- 
+
     def __init__(self,comport='COM4'):
         """Open the selected serial port"""
         self.port = serial.Serial()
@@ -26,33 +26,33 @@ class DualStepper:
             print('port opened')
         else:
             print("Select a COMPORT")
-        
-    
+
+
     def quit_serial(self):
         """Method to close the serial port when the Tk window is closed"""
         self.port.close()
         time.sleep(1)
-        
-        
+
+
     def send_command(self,cmd,steps=0):
         '''
-        The barriers move by sending messages to the arduino code newStepperBoard which handles the hardware. 
+        The barriers move by sending messages to the arduino code newStepperBoard which handles the hardware.
         The commands are as follows.
-    
+
         RX - Reset the X barrier position to zero by moving until it hits the limit switch
         RY - Reset the Y barrier position to zero by moving until it hits the limit switch
         MX+10 - Move X barrier towards middle by 10 steps
         MX-10 - Move barrier towards end by 10 steps
         MY+10 - Move Y barrier towards the middle by 10 steps
         MY-10 - Move Y barrier towards the end by 10 steps
-    
+
         To enter the command supply a string to cmd of RX,RY, MX+,MX-,MY+,MY- etc
         Supply the number of steps to move
-    
+
         Function returns the new position
         '''
 
-    
+
         if cmd =='RX':
             print('RX')
             self.port.write(b'RX\n')
@@ -89,25 +89,35 @@ class DualStepper:
             self.port.write(bytes(str(steps),'utf8'))
             self.port.write(b'\n')
             self.posy=self.posy - steps
-    
-          
+
+
         print('values written')
         time.sleep(0.5)
-        
 
- 
-    
+class Stepper(arduino.Arduino):
+
+    def __init__(self):
+        super().__init__()
+
+    def move_motor(self, motor_no, steps, direction):
+        motor_assignment = {1: 'A', 2:'B', 3:'C'}
+        message = 'M' + motor_assignment[1] + direction + str(steps) + '\n'
+        self.send_serial_line(message)
+        print('done')
+
+
 if __name__ == '__main__':
 
-    s=DualStepper()
-    
-    #Reset y barrier
-    s.send_command('RY')
+    class_choice = input("Enter '1' to test DualStepper or '2' to test Stepper ")
+    if class_choice == '1':
+        s=DualStepper()
+        #Reset y barrier
+        s.send_command('RY')
+        #Move y barrier 2000 steps towards the middle
+        s.send_command('MY+',steps=1000)
+        s.quit_serial()
 
-        
-    #Move y barrier 2000 steps towards the middle
-    s.send_command('MY+',steps=1000)
-    
-    s.quit_serial()
-    
-        
+    elif class_choice == '2':
+        s = Stepper()
+        s.move_motor(1, 100, '+')
+        s.quit_serial()
