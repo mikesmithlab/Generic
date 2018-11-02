c8bc3024b7e937673989270b0587c1cbf19f7681
James Downs <james.downs@nottingham.ac.uk>
2018-11-02 10:34:49 +0000

    Added colours to module

--- images.py
+++ images.py
@@ -2,6 +2,25 @@ import cv2
 import numpy as np
 import Generic.filedialogs as fd
 
+# Color list BGR tuples
+BLUE = (255, 0, 0)
+LIME = (0, 255, 0)
+RED = (0, 0, 255)
+YELLOW = (0, 255, 255)
+ORANGE = (0, 128, 255)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+MAGENTA = (255, 0, 255)
+PINK = MAGENTA
+CYAN = (255, 255, 0)
+NAVY = (128, 0, 0)
+TEAL = (128, 128, 0)
+PURPLE = (128, 0, 128)
+GREEN = (0, 128, 0)
+MAROON = (0, 0, 128)
+
+
+
 
 def get_width_and_height(img):
     width = get_width(img)
