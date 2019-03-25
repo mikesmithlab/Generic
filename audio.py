ca8095a6c290d65f87e478c1f0c06cf501312f18
James <jamesdownslab@gmail.com>
2019-03-25 13:24:13 +0000

    Reading audio uses moviepy rather than ffmpeg

--- audio.py
+++ audio.py
@@ -2,38 +2,11 @@ import os
 import subprocess
 from scipy.io.wavfile import read
 import numpy as np
+from moviepy.editor import AudioFileClip
+import matplotlib.pyplot as plt
 
 """Functions for extracting the frequencies of square waves in a 
 sound file."""
-
-def extract_wav(file):
-    """
-    Extract a wav file from an mp4
-
-    Parameters
-    ----------
-    file: str
-        Full path to the mp4
-
-    Returns
-    -------
-    wav: list
-        wav[0] = rate Hz
-        wav[1] = audio signal
-    """
-    out_file = os.path.splitext(file)[0] + '.wav'
-    if os.path.exists(out_file):
-        pass
-    else:
-        command = "ffmpeg -i '{}' -ab 160k -ac 2 -ar 44100 -vn '{}'".format(
-            file, out_file)
-        subprocess.call(command, shell=True)
-    wav = read_wav(out_file)
-    return wav
-
-def read_wav(file):
-    return read(file)
-
 
 def digitise(sig):
     """Makes a noisy square signal, perfectly square."""
@@ -52,10 +25,10 @@ def fourier_transform_peak(sig, time_step):
     return abs(freq[peak])
 
 
-def frame_frequency(wave, audio_rate, video_rate):
+def frame_frequency(wave, frames, audio_rate):
     """Returns the peak frequency in an audio file for each video frame"""
-    window = int(audio_rate/video_rate)
-    windows = int(np.ceil(len(wave)/window))
+    window = int(len(wave)/frames)
+    windows = frames
     freq = np.zeros(windows)
     for i in range(windows):
         b = i*window
@@ -64,3 +37,9 @@ def frame_frequency(wave, audio_rate, video_rate):
             t = len(wave)
         freq[i] = int(fourier_transform_peak(wave[b:t], 1/audio_rate))
     return freq
+
+
+def extract_wav(file):
+    audioclip = AudioFileClip(file)
+    audioclip_arr = audioclip.to_soundarray(fps=48000, nbytes=2)
+    return audioclip_arr
