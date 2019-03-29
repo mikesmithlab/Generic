21a2469ad53d963c0f4eda76761899a7aa1a4b54
James <jamesdownslab@gmail.com>
2019-03-29 12:22:10 +0000

    Fit hex speedups

--- images/contours.py
+++ images/contours.py
@@ -1,10 +1,7 @@
 import cv2
 import numpy as np
 from math import pi, cos, sin
-import matplotlib.pyplot as plt
-import scipy.spatial as sp
 import scipy.optimize as op
-from shapely.geometry import Polygon, Point
 
 __all__ = ['find_contours', 'sort_contours', 'find_contour_corners', 'fit_hex']
 
@@ -84,26 +81,31 @@ def fit_hex(contour):
     and the hexagon.
     """
     (xc, yc), radius = cv2.minEnclosingCircle(contour)
-    res = op.minimize(distance_to_hexagon, (xc, yc, radius, 0), args=contour)
+    res = op.minimize(hex_dist, (xc, yc, radius, 0), args=contour)
     (xc, yc, r, theta) = res.x
-    _, hex_corners = hexagon(xc, yc, r, theta)
-    return np.array(hex_corners)
-
-
-def distance_to_hexagon(params, contour):
-    xc, yc, r, theta = params
-    hex, _ = hexagon(xc, yc, r, theta)
-    dists = [hex.distance(Point(pnt)) for pnt in contour]
-    return np.sum(dists)
+    hex_corners = hexagon(xc, yc, r, theta)
+    return hex_corners
 
 
 def hexagon(xc, yc, r, theta):
-    points = [[xc + r*cos(t + theta), yc + r*sin(t + theta)]
-              for t in [0, pi/3, 2*pi/3, pi, 4*pi/3, 5*pi/3]]
-    hex = Polygon(points)
-    return hex, points
+    points = [[xc + r * cos(t + theta), yc + r * sin(t + theta)]
+              for t in [0, pi / 3, 2 * pi / 3, pi, 4 * pi / 3, 5 * pi / 3]]
+    return np.array(points)
 
 
+def hex_dist(params, contour):
+    """https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line"""
+    xc, yc, r, theta = params
+    points = hexagon(xc, yc, r, theta)
+    distances = np.zeros((len(contour), 6))
+    x0, y0 = np.split(contour, 2, axis=1)
+    for p in range(6):
+        x1, y1 = points[p-1, :]
+        x2, y2 = points[p, :]
+        distance = np.abs((y2-y1)*x0 - (x2-x1)*y0 + x2*y1 - y2*x1) / np.sqrt((y2-y1)**2 + (x2-x1)**2)
+        distances[:, p] = np.squeeze(distance)
+    distances = np.min(distances, axis=1)
+    return np.sum(distances)
 
 
 
