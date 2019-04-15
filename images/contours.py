224482651bf58d26f986f012fa78a8db44612f17
James <jamesdownslab@gmail.com>
2019-04-15 15:26:09 +0100

    Works on any version of opencv

--- images/contours.py
+++ images/contours.py
@@ -10,8 +10,13 @@ def find_contours(img, hierarchy=False):
     '''
     contours is a tuple containing (img, contours)
     '''
-    contours, hier = cv2.findContours(
-        img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # work for any version of opencv
+    try:
+        im, contours, hier = cv2.findContours(
+            img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    except:
+        contours, hier = cv2.findContours(
+            img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     if hierarchy:
         return contours, hier
     else:
