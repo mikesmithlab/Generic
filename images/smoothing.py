df56373ad81413e8c4982954b8b262f8f9c3f973
mikesmithlab <mike.i.smith@nottingham.ac.uk>
2019-08-29 10:46:06 +0100

    Added medianBlur to Smoothing

--- images/smoothing.py
+++ images/smoothing.py
@@ -25,3 +25,25 @@ def gaussian_blur(img, kernel=(3, 3)):
     """
     out = cv2.GaussianBlur(img, kernel, 0)
     return out
+
+def median_blur(img, kernel=(3,3)):
+    """
+    Blurs an image using a median filter
+
+    The function convolves the source image with the specified kernel.
+
+    Parameters
+    ----------
+    img: input image
+        Can have any number of channels which are processed separately
+
+    kernel: tuple giving (width, height) for kernel
+        Width and height should be positive and odd
+
+    Returns
+    -------
+    out: output image
+        Same size and type as img
+    """
+    out = cv2.medianBlur(img, kernel, 0)
+    return out
