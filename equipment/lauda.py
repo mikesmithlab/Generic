30e9c3649e6525b8d0051a6769945c6028454041
James <jamesdownslab@gmail.com>
2019-09-13 14:21:55 +0100

    Added module for interfacing with lauda temp controller

---
+++ equipment/lauda.py
@@ -0,0 +1,48 @@
+import time
+
+import numpy as np
+import serial
+
+
+class Lauda(serial.Serial):
+
+    def __init__(self, port):
+        super().__init__(port)
+        self.read_all()
+
+    def read_current_temp(self):
+        try:
+            self.read_all()
+            self.write(b'IN_PV_01\r\n')
+            time.sleep(0.3)
+            txt = self.read_all()
+            val = float(txt)
+        except:
+            val = np.NaN
+        return val
+
+    def start(self):
+        self.read_all()
+        self.write(b'START\r\n')
+
+    def stop(self):
+        self.read_all()
+        self.write(b'STOP\r\n')
+
+    def set_temp(self, new_temp):
+        self.read_all()
+        self.write(bytes('OUT_SP_00_{:06.2f}\r\n'.format(new_temp),
+                         encoding='utf-8', errors='strict'))
+
+    def set_pumping_speed(self, val):
+        self.read_all()
+        self.write(bytes('OUT_SP_01_{:03d}\r\n'.format(val),
+                         encoding='utf-8', errors='strict'))
+
+
+if __name__ == "__main__":
+    l = Lauda(
+        '/dev/serial/by-id/usb-LAUDA_DR._R._WOBSER_GMBH_CO.KG_LAUDA_Constant_Temperature_Equipment_Virtual_COM_Port_34FFD90543523238-if00')
+
+    print(l.read_current_temp())
+    l.close()
